137b5345f54d5ac7b52147128be74da2fee14abc
vinicius gularte martin <viniciusgulartemartin@gmail.com>
2018-09-05 00:30:05 -0300

    upgrade refactory

--- config/server.js
+++ config/server.js
@@ -1,28 +1,28 @@
-var bodyParser = require('body-parser');
-
 var express = require('express');
 
 var consign = require('consign');
+
+var bodyParser = require('body-parser');
 
 var expressValidator = require('express-validator');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
-
-app.use(express.static('./app/public'));
 app.use(expressValidator());
 
-consign({cwd: process.cwd() + '/app'})
-  .include('controllers')
-  .then('routes')
-.into(app);
+consign()
+    .include('app/routes')
+    .then('app/controllers')
+    .into(app);
 
 
 
