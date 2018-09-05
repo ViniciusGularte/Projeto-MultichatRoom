311f76ef0b62a9cd28c099fe50d3513449241c54
vinicius gularte martin <viniciusgulartemartin@gmail.com>
2018-09-05 01:20:57 -0300

    add socket configuration heroku

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var app = require('./config/server');
 var server = app.listen(process.env.PORT || 3000, function(){
 	console.log('Servidor Online')
 })
-
-var io = require('socket.io').listen(server);
+var server2 = app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+var io = require('socket.io').listen(server2);
 app.set('io',io);
 
 io.on('connection', function(socket){
