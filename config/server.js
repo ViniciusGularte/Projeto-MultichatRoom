5b17a47ab31b53e1fdf96cca35f0753f871f13c8
vinicius gularte martin <viniciusgulartemartin@gmail.com>
2018-09-04 23:32:11 -0300

    try path relative in heroku again 8

--- config/server.js
+++ config/server.js
@@ -1,22 +1,22 @@
-var express = require('express');
-//const path = require('path');
-var consign = require('consign');
-
 var bodyParser = require('body-parser');
+
+var express = require('express');
+
+var consign = require('consign');
 
 var expressValidator = require('express-validator');
 
 var app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
-
-app.use(express.static('./app/public'));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
+app.set('view engine', 'ejs');
+app.set('views', './app/views');
+
+app.use(express.static('./app/public'));
 app.use(expressValidator());
 
 consign({cwd: 'app'})
